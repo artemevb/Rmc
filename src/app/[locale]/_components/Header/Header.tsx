47ba09a0d76814bd544f972f390b2c03745b797a
Andrey <aa71555443@gmail.com>
2024-10-08 14:35:33 +0500

    fixed

--- src/app/[locale]/_components/Header/Header.tsx
+++ src/app/[locale]/_components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "@/src/app/[locale]/_components/Header/Logo";
 import Navigation from "@/src/app/[locale]/_components/Header/Navigation";
 import Tools from "@/src/app/[locale]/_components//Header/Tools";
 
-export default function Header({ locale }) {
+export default function Header() {
   const data = [
     {
       title: 'Услуги',
@@ -26,8 +26,8 @@ export default function Header({ locale }) {
     <header className="w-full bg-white px-2 h-[90px] max-mdx:h-[72px] xl:h-[100px] shadow-2xl">
       <div className="w-full max-w-[1440px] flex items-center justify-between gap-2 h-full mx-auto ">
         <Logo />
-        <Navigation navOptions={data} locale={locale}/>
-        <Tools navOptions={data} />
+        <Navigation navOptions={data} />
+        <Tools />
       </div>
     </header>
   )
