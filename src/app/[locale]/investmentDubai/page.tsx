848933db22df53666405fe500f885fc4147b72bb
Andrey <aa71555443@gmail.com>
2024-10-09 19:00:39 +0500

    fix

--- src/app/[locale]/investmentDubai/page.tsx
+++ src/app/[locale]/investmentDubai/page.tsx
@@ -8,12 +8,18 @@ import PopularReviews from "../_components/InvestmentDubai/PopularReviews";
 import Counter from "../_components/Main/Counter";
 import Form from "../_components/Main/Form";
 import BlogSlider from "../_components/Main/Blogs";
+import type { Locales } from "@/src/app/[locale]/layout";
 
-interface NewsCompProps {
-  locale: string;
-}
+type InvestmentDubaiPageProps = {
+  params: {
+    locale?: string;
+  };
+};
 
-export default function Main({ locale }: NewsCompProps) {
+export default function InvestmentDubaiPage({ params }: InvestmentDubaiPageProps) {
+  const locale: Locales = params?.locale === 'uz' ? 'uz'
+    : params?.locale === 'en' ? 'en'
+      : 'ru';
   return (
     <div className=" bg-white flex flex-col gap-[120px] mdl:gap-[150px] xl:gap-[200px]">
       <Banner />
