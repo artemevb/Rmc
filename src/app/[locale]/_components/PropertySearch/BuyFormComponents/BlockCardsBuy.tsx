d235030aaf334b6e74e890d08f3abf0ce0395425
Andrey <aa71555443@gmail.com>
2024-12-23 15:58:12 +0500

    t

--- src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
+++ src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
@@ -18,19 +18,19 @@ export default function BlockCardsBuy() {
             </p>
             <div className="w-full flex flex-col 3xl:flex-row gap-[16px] mt-[40px] xl:mt-[50px] 3xl:grid 3xl:grid-cols-3">
                 <div className="w-full flex flex-col gap-[16px] mdl:flex-row">
-                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px] 3xl:h-[540px] relative overflow-hidden'>
-                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card1_title')}</h3>
-                        <div className='absolute bottom-[35px] 3xl:bottom-[-30px] right-[-20px] h-[45%]'>
-                            <Image
-                                src={rent1}
-                                quality={100}
-                                width={1500}
-                                height={1500}
-                                alt={t('card1_image_alt')}
-                                className="w-full h-full object-cover min-h-[225px] "
-                            />
-                        </div>
-                    </div>
+                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px] md:h-[400px] 3xl:h-[540px] relative overflow-hidden'>
+    <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card1_title')}</h3>
+    <div className='absolute bottom-[35px] right-0 md:right-[-20px] h-[45%]'>
+        <Image
+            src={rent1}
+            quality={100}
+            width={1500}
+            height={1500}
+            alt={t('card1_image_alt')}
+            className="w-full h-full object-cover min-h-[225px]"
+        />
+    </div>
+</div>
                     {/** При 3xl: скрывается,т.к иначе отображается в начале*/}
                     <div className='w-full pt-[24px] px-[20px] bg-[#EDF3F5] h-[350px] 3xl:hidden relative overflow-hidden'>
                         <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[336px]'>{t('card2_title')}</h3>
