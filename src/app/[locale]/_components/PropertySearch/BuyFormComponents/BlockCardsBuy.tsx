2a8699e9c67bb9b3d8168ad92e0281d6b78e6246
Andrey <aa71555443@gmail.com>
2024-12-23 15:59:10 +0500

    te

--- src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
+++ src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
@@ -61,9 +61,9 @@ export default function BlockCardsBuy() {
                             />
                         </div>
                     </div>
-                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px] 3xl:h-[260px] relative overflow-hidden'>
+                    <div className='w-full pt-[24px] px-[20px] bg-[#EDF3F5] h-[350px] 3xl:h-[260px] relative overflow-hidden'>
                         <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[336px]'>{t('card4_title')}</h3>
-                        <div className='absolute bottom-[-20px] right-0 h-[60%]'>
+                        <div className='absolute bottom-[0px] right-[-40px] 3xl:right-[-10px] h-[70%] '>
                             <Image
                                 src={divan}
                                 quality={100}
