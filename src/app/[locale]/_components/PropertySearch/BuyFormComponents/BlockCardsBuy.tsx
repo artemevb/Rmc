8c4ce26c1c4ca3b6c2ed7bd6a8bd52ef03d814a1
Andrey <aa71555443@gmail.com>
2024-12-23 16:09:41 +0500

    new

--- src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
+++ src/app/[locale]/_components/PropertySearch/BuyFormComponents/BlockCardsBuy.tsx
@@ -16,21 +16,21 @@ export default function BlockCardsBuy() {
             <p className="text-[16px] mdx:text-[20px] text-[#858585] max-w-[817px] mt-[12px] mdx:mt-[16px]">
                 {t('description')}
             </p>
-            <div className="w-full flex flex-col 3xl:flex-row gap-[16px] mt-[40px] xl:mt-[50px] 3xl:grid 3xl:grid-cols-3">
+            <div className="w-full  hidden flex-col 3xl:flex-row gap-[16px] mt-[40px] xl:mt-[50px] 3xl:grid 3xl:grid-cols-3">
                 <div className="w-full flex flex-col gap-[16px] mdl:flex-row">
-                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px] md:h-[400px] 3xl:h-[540px] relative overflow-hidden'>
-    <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card1_title')}</h3>
-    <div className='absolute bottom-[35px] right-0 md:right-[-20px] h-[45%]'>
-        <Image
-            src={rent1}
-            quality={100}
-            width={1500}
-            height={1500}
-            alt={t('card1_image_alt')}
-            className="w-full h-full object-cover min-h-[225px]"
-        />
-    </div>
-</div>
+                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px] 3xl:h-[540px] relative overflow-hidden'>
+                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card1_title')}</h3>
+                        <div className='absolute bottom-[35px] 3xl:bottom-[-30px] right-[-20px] h-[45%]'>
+                            <Image
+                                src={rent1}
+                                quality={100}
+                                width={1500}
+                                height={1500}
+                                alt={t('card1_image_alt')}
+                                className="w-full h-full object-cover min-h-[225px] "
+                            />
+                        </div>
+                    </div>
                     {/** При 3xl: скрывается,т.к иначе отображается в начале*/}
                     <div className='w-full pt-[24px] px-[20px] bg-[#EDF3F5] h-[350px] 3xl:hidden relative overflow-hidden'>
                         <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[336px]'>{t('card2_title')}</h3>
@@ -87,6 +87,77 @@ export default function BlockCardsBuy() {
                             alt={t('card5_image_alt')}
                             className="w-full h-full object-cover"
                         />
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+            <div className="w-full flex flex-col gap-[16px] mt-[40px] xl:mt-[50px] 3xl:hidden">
+                <div className="w-full flex flex-col gap-[16px] mdl:flex-row">
+                    <div className='w-full h-full pt-[24px] pl-[24px] bg-[#EDF3F5] min-h-[350px] relative overflow-hidden'>
+                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card1_title')}</h3>
+                        <div className='absolute bottom-[35px] right-[-20px] h-[45%]'>
+                            <Image
+                                src={rent1}
+                                quality={100}
+                                width={1500}
+                                height={1500}
+                                alt={t('card1_image_alt')}
+                                className="w-full h-full object-cover min-h-[225px] "
+                            />
+                        </div>
+                    </div>
+
+                    <div className='w-full pt-[24px] px-[20px] bg-[#EDF3F5] h-[350px] relative overflow-hidden'>
+                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[336px]'>{t('card2_title')}</h3>
+                        <div className='absolute bottom-[0px] right-0 w-[66%]'>
+                            <Image
+                                src={rent3}
+                                quality={100}
+                                width={1500}
+                                height={1500}
+                                alt={t('card2_image_alt')}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-[16px]">
+                    <div className='w-full pt-[24px] pl-[24px] bg-[#EDF3F5] h-[350px]  relative overflow-hidden'>
+                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[294px]'>{t('card3_title')}</h3>
+                        <div className='absolute bottom-[-20px] right-0 h-[60%]'>
+                            <Image
+                                src={Illustration}
+                                quality={100}
+                                width={1500}
+                                height={1500}
+                                alt={t('card3_image_alt')}
+                                className="w-full h-full object-cover "
+                            />
+                        </div>
+                    </div>
+                    <div className='w-full pt-[24px] px-[20px] bg-[#EDF3F5] h-[350px] relative overflow-hidden'>
+                        <h3 className='text-[18px] mdx:text-[24px] top-0 max-w-[336px]'>{t('card4_title')}</h3>
+                        <div className='absolute bottom-[0px] right-[-40px] h-[70%] '>
+                            <Image
+                                src={divan}
+                                quality={100}
+                                width={1500}
+                                height={1500}
+                                alt={t('card4_image_alt')}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
