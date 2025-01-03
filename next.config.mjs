dd1ac2d06cfc194fb58ce696fb52cf210e038a67
Andrey <aa71555443@gmail.com>
2025-01-03 17:53:06 +0500

    Warning:update next.config and page property search

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.js
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
@@ -8,6 +9,29 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['rmc.mrjtrade.uz', 'cdn.sanity.io'], // Добавьте домен, откуда будут загружаться изображения
+  },
+  async headers() {
+    return [
+      {
+        // Применять к API маршрутам
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+      // Другие правила заголовков при необходимости
+    ];
   },
 };
 
--- src/app/[locale]/property-search/page.tsx
+++ src/app/[locale]/property-search/page.tsx
@@ -1,8 +1,8 @@
 // src/app/[locale]/page.tsx
-import { client } from '@/src/sanity/lib/client';
 import { GET_RESIDENTIAL_COMPLEXES, GET_LAYOUTS } from '../_components/PropertySearch/queries';
 import PageContent from '../_components/PropertySearch/PageContent';
 import { Locale } from '@/src/app/[locale]/_components/PropertySearch/locale';
+import { apiVersion, dataset, projectId } from '@/src/sanity/env'; // Импортируем переменные окружения
 
 interface PageProps {
   params: {
@@ -19,10 +19,34 @@ export default async function Page({ params }: PageProps) {
     locale = 'en'; 
   }
 
-  const complexes = await client.fetch(GET_RESIDENTIAL_COMPLEXES);
-  const layouts = await client.fetch(GET_LAYOUTS);
+  const GET_RESIDENTIAL_COMPLEXES_QUERY = encodeURIComponent(GET_RESIDENTIAL_COMPLEXES);
+  const GET_LAYOUTS_QUERY = encodeURIComponent(GET_LAYOUTS);
+
+  const SANITY_API_URL = `https://${projectId}.api.sanity.io/v${apiVersion}/data/query/${dataset}`;
+
+  // Функция для выполнения запросов к Sanity с отключенным кешированием
+  const fetchSanityData = async (query: string) => {
+    const response = await fetch(`${SANITY_API_URL}?query=${query}`, {
+      method: 'GET',
+      cache: 'no-store', // Отключение кеширования
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.result;
+  };
+
+  // Получение данных параллельно
+  const [complexes, layouts] = await Promise.all([
+    fetchSanityData(GET_RESIDENTIAL_COMPLEXES_QUERY),
+    fetchSanityData(GET_LAYOUTS_QUERY),
+  ]);
 
   return (
     <PageContent complexes={complexes} layouts={layouts} locale={locale as Locale} />
   );
 }
+
